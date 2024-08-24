accdf00123126f4f5e4902bfc16a513f7c40fd38
Massamba505 <sabelomaphalala131@gmail.com>
2024-08-24 11:55:57 +0200

    blogging-post

--- project-management-tool/backend/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {protectRoute} = require('../middlewares/protectRoute');
-const { all } = require('../controllers/user.controller');
-
-router.get('/all', all);
-
-module.exports = router;
