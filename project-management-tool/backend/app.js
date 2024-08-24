689f54f1ed96dc82f18d6b8a9d8fd90a9fe61499
Massamba505 <sabelomaphalala131@gmail.com>
2024-08-24 07:28:50 +0200

    cors

--- project-management-tool/backend/app.js
+++ project-management-tool/backend/app.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookierParser());
+app.use(cors());
 
 app.use("/api/auth/",authRoutes);
 app.use('/api/projects', projectRoutes);
